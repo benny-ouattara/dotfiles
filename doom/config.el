4c98253c76a8da10bfe1949207159bdd18e9568e
Ben O <zangao@spotify.com>
2021-11-17 15:51:49 -0500

    disable lsp ui mode by default

--- doom/config.el
+++ doom/config.el
@@ -295,6 +295,7 @@ and org files on the top right. Keeps current window on the left."
 ;; makes lsp-mode a little more bearable: hide all the UI noise
 (setq lsp-ui-sideline-show-code-actions nil)
 (setq lsp-ui-doc-mode nil)
+(lsp-ui-doc-mode -1)
 
 (setq java-dir "/Library/Java/JavaVirtualMachines")
 (setq java-home-suffix "/Contents/Home")
