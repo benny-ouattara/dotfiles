57030f31e7faf1b4d954cafddd8e55e0f0d1911d
Ben O <zangao@spotify.com>
2022-07-15 09:24:25 -0400

    rlwrap guile

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -150,6 +150,7 @@ in rec {
                + " | ${pkgs.gnugrep}/bin/egrep -v \"(lo0|vmnet|169\\.254|255\\.255)\""
                + " | ${pkgs.coreutils}/bin/tail -n +5";
       sbcl  = "${pkgs.rlwrap}/bin/rlwrap sbcl";
+      guile  = "${pkgs.rlwrap}/bin/rlwrap guile";
       info  = "info --vi-keys";
     };
 
