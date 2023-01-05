94d7835d84723d1b8c52a11016d033d92c3d7e07
Ben O <benny.ouattara@gmail.com>
2023-01-04 22:12:54 -0500

    Use substitutes when pulling from channel

--- guix/channels.scm
+++ guix/channels.scm
@@ -1,12 +1,14 @@
+(use-modules (guix ci))
+
 (list (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-       )
-      (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix")
-        (introduction
-          (make-channel-introduction
-            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-            (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
+       (name 'nonguix)
+       (url "https://gitlab.com/nonguix/nonguix")
+       (branch "master")
+       (introduction
+        (make-channel-introduction
+         "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+         (openpgp-fingerprint
+          "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel-with-substitutes-available
+       %default-guix-channel
+       "https://ci.guix.gnu.org"))
