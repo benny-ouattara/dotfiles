05a2c7bf602e99ccec5633b332bff094b4a6f9e8
Ben Ouattara <benny.ouattara@gmail.com>
2023-06-24 10:02:27 -0400

    Place cider-repl to the right

--- doom/config.el
+++ doom/config.el
@@ -937,6 +937,7 @@ $stderr = File.open(\"err.txt\", \"w\")")
       (set-popup-rule! "*kubel" :size 0.50 :vslot -4 :select t :quit nil :ttl t :side 'right)
       (set-popup-rule! "^\\*sbt*" :size 0.40 :vslot -4 :select t :quit nil :ttl nil :side 'right)
       (set-popup-rule! "^\\*cider.*" :size 0.40 :vslot -4 :select t :quit nil :ttl t :side 'right)
+      (set-popup-rule! "^\\*cider-repl.*" :size 0.40 :vslot -4 :select t :quit nil :ttl t :side 'right)
       (set-popup-rule! "^\\*ChatGPT*" :size 0.40 :vslot -4 :select t :quit nil :ttl nil :side 'right))
   ;; small display
   (progn
@@ -955,6 +956,7 @@ $stderr = File.open(\"err.txt\", \"w\")")
     (set-popup-rule! "*kubel" :size 0.35 :vslot -4 :select t :quit nil :ttl t :side 'bottom)
     (set-popup-rule! "^\\*sbt*" :size 0.35 :vslot -4 :select t :quit nil :ttl nil :side 'bottom)
     (set-popup-rule! "^\\*cider.*" :size 0.35 :vslot -4 :select t :quit nil :ttl t :side 'bottom)
+    (set-popup-rule! "^\\*cider-repl.*" :size 0.35 :vslot -4 :select t :quit nil :ttl t :side 'bottom)
     (set-popup-rule! "^\\*ChatGPT*" :size 0.35 :vslot -4 :select t :quit nil :ttl nil :side 'bottom)))
 
 ;; (vertico-posframe-mode 1)
