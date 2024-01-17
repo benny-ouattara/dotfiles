e70b6d4f741e15b3697aac04acf90289e43abbd3
Ben Ouattara <benny.ouattara@gmail.com>
2024-01-17 11:48:21 -0500

    emacs: build with xwidget support

--- nixpkgs/darwin/mac-config.nix
+++ nixpkgs/darwin/mac-config.nix
@@ -82,7 +82,7 @@ in
         ];
         extraConfig = ''
       # cask_args appdir: "${home-dir}/Applications"
-      brew "emacs-plus@29", args: ["with-imagemagick", "with-modern-sexy-v2-icon"], link: true
+      brew "emacs-plus@29", args: ["with-imagemagick", "with-modern-sexy-v2-icon", "with-xwidgets"], link: true
     '';
     };
 
