63af1b1056350823d7d847c0a138c39c33e91064
Ben O <zangao@spotify.com>
2024-03-18 16:03:47 -0400

    python: remove lsp

--- doom/init.el
+++ doom/init.el
@@ -32,7 +32,7 @@
                   +hydra/text-zoom/body)
            (emoji +unicode)  ; 🙂
            ;;indent-guides     ; highlighted indent columns
-           ;;  (modeline +light)          ; snazzy, Atom-inspired modeline, plus API
+           ;; (modeline +light)          ; snazzy, Atom-inspired modeline, plus API
            nav-flash                  ; blink the current line after jumping
            ;;neotree           ; a project drawer, like NERDTree for vim
            ophints                ; highlight the region an operation acts on
@@ -172,7 +172,7 @@
            ;;php               ; perl's insecure younger brother
            ;;plantuml          ; diagrams for confusing people more
            ;;purescript        ; javascript, but functional
-           (python +lsp)   ; beautiful is better than ugly
+           python              ; beautiful is better than ugly
            ;;qt                ; the 'cutest' gui framework ever
            ;;racket            ; a DSL for DSLs
            ;;raku              ; the artist formerly known as perl6
