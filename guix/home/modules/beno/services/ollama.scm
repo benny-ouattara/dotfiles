ffc21448d7100d97280043613aaebbb5feb570cb
Ben O. <ben.ouattara@proton.me>
2026-03-10 22:23:44 -0400

    otter: ollama service

--- guix/home/modules/beno/services/ollama.scm
+++ guix/home/modules/beno/services/ollama.scm
@@ -1,30 +1,45 @@
 (define-module (beno services ollama)
   #:use-module (guix gexp)
+  #:use-module (guix records)                 ; for match-record
   #:use-module (gnu home services)            ; for service-type
   #:use-module (gnu home services shepherd)   ; for home-shepherd-service-type
   #:use-module (px packages ai)               ; for ollama
-  #:export (home-ollama-service-type))
+  #:export (home-ollama-service-type
+            ollama-configuration
+            ollama-test))
+
+(define-record-type* <ollama-configuration>
+  ollama-configuration
+  make-ollama-configuration
+  ollama-configuration?
+  (package ollama-package
+           (default "/home/ben/.nix-profile/bin/ollama"))
+  (env ollama-env
+       (default (list "HOME=/home/ben"
+                      "OLLAMA_HOST=0.0.0.0"
+                      "OLLAMA_DEBUG=4"
+                      "HSA_OVERRIDE_GFX_VERSION=11.0.2"
+                      "OLLAMA_MODELS=/home/ben/.ollama/models")))
+  (log ollama-log
+       (default "/home/ben/.local/state/log/ollama.logs")))
 
 ;; "OLLAMA_KEEP_ALIVE=24h"
 (define (home-ollama-shepherd-service config)
-  (list (shepherd-service
-          (provision '(ollama))
-          (documentation "Start the ollama server")
-          (start #~(make-forkexec-constructor
-                    (list "/home/ben/.nix-profile/bin/ollama" "serve")
-                    #:environment-variables (list "HOME=/home/ben"
-                                                  "OLLAMA_HOST=0.0.0.0"
-                                                  "OLLAMA_DEBUG=4"
-                                                  "HSA_OVERRIDE_GFX_VERSION=11.0.2"
-                                                  "OLLAMA_MODELS=/home/ben/.ollama/models")
-                    #:log-file "/home/ben/.local/state/log/ollama.logs"))
-          (stop #~(make-kill-destructor)))))
+  (match-record config <ollama-configuration> (package env log)
+    (list (shepherd-service
+            (provision '(ollama))
+            (documentation "Start the ollama server")
+            (start #~(make-forkexec-constructor
+                      (list #$package "serve")
+                      #:environment-variables #$env
+                      #:log-file #$log))
+            (stop #~(make-kill-destructor))))))
 
 (define-public home-ollama-service-type
   (service-type (name 'ollama)
                 (extensions (list (service-extension
                                    home-shepherd-service-type
                                    home-ollama-shepherd-service)))
-                (default-value #f)
+                (default-value (ollama-configuration))
                 (description
                  "Launch the ollama server so running ollama works out of the box.")))
