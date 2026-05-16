fba0dda8d868cc986ed8f3a4b7637c99e61b5f75
Ben O. <ben.ouattara@proton.me>
2026-05-15 22:32:46 -0400

    Remove legacy pre-flake nixpkgs config
    System now runs entirely from nix-darwin/flake.nix

--- nixpkgs/default.nix
+++
@@ -1,5 +0,0 @@
-{ nixpkgs ? import <nixpkgs>, system ? builtins.currentSystem
-, pkgs ? import <nixpkgs> { inherit system; }, configuration ? <darwin-config>
-, darwin ? import <darwin> { inherit configuration system pkgs; } }: {
-  nix-darwin = darwin.system;
-}
