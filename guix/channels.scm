61af08bf4879a9946eeb4fb12358e3a77b811577
Ben O. <ben.ouattara@proton.me>
2026-09-12 16:37:47 -0400

    Update Guix channels and adapt configs to upstream changes
    - Bump guix, nonguix and pantherx channel commits
    - Pass --unsafe-channel-evaluation so channels.scm can read GITHUB_TOKEN
      (Guix now sandboxes channel files); restore jazacash channel
    - Import stumpwm packages from (gnu packages window-management) and use
      -qt5 SDDM theme variants to silence deprecation warnings
    - Drop version pins from home packages; disable jtools
    - Remove unused mpd, ncmpcpp, nixpkgs, nushell and qutebrowser configs

--- guix/channels.scm
+++ guix/channels.scm
@@ -1,41 +1,38 @@
-(use-modules (guix ci))
-
-(define %token (getenv "GITHUB_TOKEN"))
-(define %repo (string-append "https://jazafund:" %token "@github.com/jazafund/jazacash.git"))
-
 (list (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix")
-        (branch "master")
-        (commit
-         "1980960f932063f42f97ad3be4b020f68d24e62b")
-        (introduction
-         (make-channel-introduction
-          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-          (openpgp-fingerprint
-           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+       (name 'nonguix)
+       (url "https://gitlab.com/nonguix/nonguix")
+       (branch "master")
+       (commit
+        "a89286d75f8dcadeabcf807eb259203aff259a63")
+       (introduction
+        (make-channel-introduction
+         "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+         (openpgp-fingerprint
+          "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      ;; Guix sandboxes channel files; getenv needs --unsafe-channel-evaluation
       (channel
-        (name 'jazacash)
-        (url %repo)
-        (branch "develop"))
+       (name 'jazacash)
+       (url (string-append "https://jazafund:" (getenv "GITHUB_TOKEN")
+                           "@github.com/jazafund/jazacash.git"))
+       (branch "develop"))
       (channel
-        (name 'pantherx)
-        (url "https://codeberg.org/gofranz/panther.git")
-        (branch "master")
-        (commit "3c29b71caf60e5a2efc48287cf325776287d3a5f")
-        (introduction
-         (make-channel-introduction
-          "7e01d7fd21e20f04acbb05c51fbe8b365e70eebd"
-          (openpgp-fingerprint
-           "A36A D41E ECC7 A871 1003  5D24 524F EB1A 9D33 C9CB"))))
+       (name 'pantherx)
+       (url "https://codeberg.org/gofranz/panther.git")
+       (branch "master")
+       (commit "77ed154bd927c014f8f77d14ba493b41a082f1d8")
+       (introduction
+        (make-channel-introduction
+         "7e01d7fd21e20f04acbb05c51fbe8b365e70eebd"
+         (openpgp-fingerprint
+          "A36A D41E ECC7 A871 1003  5D24 524F EB1A 9D33 C9CB"))))
       (channel
-        (name 'guix)
-        (url "https://git.guix.gnu.org/guix.git")
-        (branch "master")
-        (commit
-         "a29122743a67a453ca74042e00d521fffcbc3310")
-        (introduction
-         (make-channel-introduction
-          "9edb3f66fd807b096b48283debdcddccfea34bad"
-          (openpgp-fingerprint
-           "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+       (name 'guix)
+       (url "https://git.guix.gnu.org/guix.git")
+       (branch "master")
+       (commit
+        "e1f2750b3d265dfb3958ea07c91630b0f9b362c2")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
--- guix/system/config.scm
+++ guix/system/config.scm
@@ -9,7 +9,7 @@
   #:use-module (gnu packages guile-xyz)
   #:use-module (gnu packages ssh)
   #:use-module (gnu packages base)
-  #:use-module (gnu packages wm)
+  #:use-module (gnu packages window-management)
   #:use-module (gnu packages shells)
   #:use-module (gnu packages docker)
   #:use-module (gnu packages xorg)
@@ -67,46 +67,46 @@
 
 (define nonguix
   (channel
-    (name 'nonguix)
-    (url "https://gitlab.com/nonguix/nonguix")
-    (branch "master")
-    (commit "1980960f932063f42f97ad3be4b020f68d24e62b")
-    (introduction
-     (make-channel-introduction
-      "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-      (openpgp-fingerprint
-       "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
+   (name 'nonguix)
+   (url "https://gitlab.com/nonguix/nonguix")
+   (branch "master")
+   (commit "a89286d75f8dcadeabcf807eb259203aff259a63")
+   (introduction
+    (make-channel-introduction
+     "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+     (openpgp-fingerprint
+      "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
 
 (define jazacash
   (channel
-    (name 'jazacash)
-    (url %repo)
-    (branch "develop")))
+   (name 'jazacash)
+   (url %repo)
+   (branch "develop")))
 
 (define panther
   (channel
-    (name 'pantherx)
-    (url "https://codeberg.org/gofranz/panther.git")
-    (branch "master")
-    (commit "0a9705def6dcc070ea142403bf836d90a0b8e6f9")
-    (introduction
-     (make-channel-introduction
-      "54b4056ac571611892c743b65f4c47dc298c49da"
-      (openpgp-fingerprint
-       "A36A D41E ECC7 A871 1003  5D24 524F EB1A 9D33 C9CB")))))
+   (name 'pantherx)
+   (url "https://codeberg.org/gofranz/panther.git")
+   (branch "master")
+   (commit "77ed154bd927c014f8f77d14ba493b41a082f1d8")
+   (introduction
+    (make-channel-introduction
+     "54b4056ac571611892c743b65f4c47dc298c49da"
+     (openpgp-fingerprint
+      "A36A D41E ECC7 A871 1003  5D24 524F EB1A 9D33 C9CB")))))
 
 (define guix
   (channel
-    (name 'guix)
-    (url "https://git.guix.gnu.org/guix.git")
-    (branch "master")
-    (commit
-     "a29122743a67a453ca74042e00d521fffcbc3310")
-    (introduction
-     (make-channel-introduction
-      "9edb3f66fd807b096b48283debdcddccfea34bad"
-      (openpgp-fingerprint
-       "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+   (name 'guix)
+   (url "https://git.guix.gnu.org/guix.git")
+   (branch "master")
+   (commit
+    "e1f2750b3d265dfb3958ea07c91630b0f9b362c2")
+   (introduction
+    (make-channel-introduction
+     "9edb3f66fd807b096b48283debdcddccfea34bad"
+     (openpgp-fingerprint
+      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
 
 (define %channels (list nonguix guix panther jazacash))
 (define %motd (plain-file "motd" "Hi Ben, welcome!\n\n"))
@@ -123,145 +123,145 @@ ben ALL=(root) NOPASSWD: ALL\n"))
 
 (define %modified-desktop-services
   (modify-services %desktop-services
-    (delete console-font-service-type) ;; provide other console fonts below
-    (delete gdm-service-type)
-    (login-service-type config =>
-                        (login-configuration (inherit config)
-                                             (motd %motd)))
-    (guix-service-type config =>
-                       (guix-configuration (inherit config)
-                                           (channels %channels)
-                                           (guix (guix-for-channels %channels))
-                                           (substitute-urls
-                                            (cons* "https://substitutes.nonguix.org"
-                                                   "https://substitutes.guix.gofranz.com"
-                                                   ;; "http://substitutes.jazacash.com"
-                                                   %default-substitute-urls))
-                                           (authorized-keys
-                                            (cons* (local-file "../keys/nonguix-key.pub")
-                                                   (local-file "../keys/pantherx-key.pub")
-                                                   (local-file "../keys/cuirass-key.pub")
-                                                   %default-authorized-guix-keys))))))
+                   (delete console-font-service-type) ;; provide other console fonts below
+                   (delete gdm-service-type)
+                   (login-service-type config =>
+                                       (login-configuration (inherit config)
+                                                            (motd %motd)))
+                   (guix-service-type config =>
+                                      (guix-configuration (inherit config)
+                                                          (channels %channels)
+                                                          (guix (guix-for-channels %channels))
+                                                          (substitute-urls
+                                                           (cons* "https://substitutes.nonguix.org"
+                                                                  "https://substitutes.guix.gofranz.com"
+                                                                  ;; "http://substitutes.jazacash.com"
+                                                                  %default-substitute-urls))
+                                                          (authorized-keys
+                                                           (cons* (local-file "../keys/nonguix-key.pub")
+                                                                  (local-file "../keys/pantherx-key.pub")
+                                                                  (local-file "../keys/cuirass-key.pub")
+                                                                  %default-authorized-guix-keys))))))
 
 (operating-system
-  (kernel linux)
-  (firmware (list linux-firmware))
-  (locale "en_US.utf8")
-  (locale-libcs (list glibc-2.35 glibc-2.39 (canonical-package glibc)))
-  (timezone "America/New_York")
-  (keyboard-layout (keyboard-layout "us"))
-  (host-name "otter")
-  (users (cons* (user-account
-                  (name "ben")
-                  (comment "Primary User")
-                  (group "users")
-                  (shell (file-append zsh "/bin/zsh"))
-                  (home-directory "/home/ben")
-                  (supplementary-groups '("cgroup" "wheel" "netdev" "audio" "video" "lp"))) 
-                %base-user-accounts))
-  (sudoers-file %sudoers)
-  (privileged-programs
-   (cons*
-    (privileged-program
-      (program (file-append stumpwm+slynk "/bin/stumpwm"))
-      (setuid? #t))
-    %default-privileged-programs))
-  (packages (cons*
-	         emacs-next
-             neovim
-	         sbcl
-	         xterm
-             openssl
-             stumpwm+slynk
-             sbcl-stumpwm-swm-gaps
-             sbcl-stumpwm-screenshot
-             sbcl-stumpwm-pass
-             sbcl-stumpwm-pamixer
-	         stumpish
-             podman
-             nix
-             tailscale
-             wireshark
-             podman-compose
-             otter-cli
-             guile-gnutls
-             guile-gcrypt
-             guile-git
-             guile-fibers
-             guile-jwt
-	         sugar-light-sddm-theme
-             dexy-color-sddm-theme
-             chili-sddm-theme
-	         %base-packages))
-  (services
-   (cons* 
-    ;; (service jazacash-secrets-service-type secrets-config)
-    ;; (service jazacash-ci-service-type)
-    (service nix-service-type
-             (nix-configuration
-               (sandbox #f)
-               (extra-config
-                '("experimental-features = nix-command flakes\n"
-                  "extra-platforms = i686-linux aarch64-linux\n"
-                  "keep-outputs = true\n"
-                  "substituters = https://cache.nixos.org/\n" 
-                  "trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=\n"
-                  "keep-derivations = true\n"))))
-    (simple-service 'profiles-files etc-profile-d-service-type
-                    (list
-                     (plain-file "mock.sh" "MOCK=1")
-                     (file-append nix "/etc/profile.d/nix-daemon.sh")
-                     (file-append nix "/etc/profile.d/nix.sh")))
-    (service iptables-service-type)        ; required for podman-service
-    (service openwebui-service-type)
-    (service openclaw-service-type)
-    (service rootless-podman-service-type
-             (rootless-podman-configuration
-               (subgids
-                (list (subid-range (name "ben"))
-                      (subid-range (name "openclaw"))
-                      (subid-range (name "openwebui"))))
-               (subuids
-                (list (subid-range (name "ben"))
-                      (subid-range (name "openclaw"))
-                      (subid-range (name "openwebui"))))))
-    (service tailscale-service-type)
-    (service syncthing-service-type
-             (syncthing-configuration (user "ben")))
-    (service gnome-desktop-service-type)
-    (service qemu-binfmt-service-type
-             (qemu-binfmt-configuration
-               (platforms (lookup-qemu-platforms "arm" "aarch64"))))
-    (service bluetooth-service-type)
-    (udev-rules-service 'brightness brightnessctl)
-    (service openssh-service-type
-             (openssh-configuration
-               (permit-root-login 'prohibit-password)
-               (password-authentication? #f)
-               (authorized-keys
-                `(("root" ,(local-file "../keys/mac.pub"))
-                  ("ben"  ,(local-file "../keys/mac.pub"))))))
-    (set-xorg-configuration
-     (xorg-configuration (keyboard-layout keyboard-layout))
-     sddm-service-type)
-    (service sddm-service-type
-             (sddm-configuration
-               ;; valid values are elarun, maldives or maya, chili, sugar-light
-               (theme "chili")))
-    %modified-desktop-services))
-  (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                (targets (list "/boot/efi"))
-                (keyboard-layout keyboard-layout)))
-  (swap-devices (list (swap-space
-                        (target (uuid "893cebe6-5e30-4588-9c99-1a03389facd8")))))
-  ;; sudo blkid to list UUIDs
-  (file-systems (cons* (file-system
-                         (mount-point "/boot/efi")
-                         (device (uuid "D47F-3FB9" 'fat32))
-                         (type "vfat"))
-                       (file-system
-                         (mount-point "/")
-                         (device (uuid "d690f4db-ddfc-45e7-9b77-c4a2e08892b3" 'ext4))
-                         (type "ext4")) %base-file-systems)))
+ (kernel linux)
+ (firmware (list linux-firmware))
+ (locale "en_US.utf8")
+ (locale-libcs (list glibc-2.35 glibc-2.39 (canonical-package glibc)))
+ (timezone "America/New_York")
+ (keyboard-layout (keyboard-layout "us"))
+ (host-name "otter")
+ (users (cons* (user-account
+                (name "ben")
+                (comment "Primary User")
+                (group "users")
+                (shell (file-append zsh "/bin/zsh"))
+                (home-directory "/home/ben")
+                (supplementary-groups '("cgroup" "wheel" "netdev" "audio" "video" "lp"))) 
+               %base-user-accounts))
+ (sudoers-file %sudoers)
+ (privileged-programs
+  (cons*
+   (privileged-program
+    (program (file-append stumpwm+slynk "/bin/stumpwm"))
+    (setuid? #t))
+   %default-privileged-programs))
+ (packages (cons*
+	        emacs-next
+            neovim
+	        sbcl
+	        xterm
+            openssl
+            stumpwm+slynk
+            sbcl-stumpwm-swm-gaps
+            sbcl-stumpwm-screenshot
+            sbcl-stumpwm-pass
+            sbcl-stumpwm-pamixer
+	        stumpish
+            podman
+            nix
+            tailscale
+            wireshark
+            podman-compose
+            otter-cli
+            guile-gnutls
+            guile-gcrypt
+            guile-git
+            guile-fibers
+            guile-jwt
+	        sugar-light-sddm-theme-qt5
+            dexy-color-sddm-theme-qt5
+            chili-sddm-theme-qt5
+	        %base-packages))
+ (services
+  (cons* 
+   ;; (service jazacash-secrets-service-type secrets-config)
+   ;; (service jazacash-ci-service-type)
+   (service nix-service-type
+            (nix-configuration
+             (sandbox #f)
+             (extra-config
+              '("experimental-features = nix-command flakes\n"
+                "extra-platforms = i686-linux aarch64-linux\n"
+                "keep-outputs = true\n"
+                "substituters = https://cache.nixos.org/\n" 
+                "trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=\n"
+                "keep-derivations = true\n"))))
+   (simple-service 'profiles-files etc-profile-d-service-type
+                   (list
+                    (plain-file "mock.sh" "MOCK=1")
+                    (file-append nix "/etc/profile.d/nix-daemon.sh")
+                    (file-append nix "/etc/profile.d/nix.sh")))
+   (service iptables-service-type)        ; required for podman-service
+   (service openwebui-service-type)
+   (service openclaw-service-type)
+   (service rootless-podman-service-type
+            (rootless-podman-configuration
+             (subgids
+              (list (subid-range (name "ben"))
+                    (subid-range (name "openclaw"))
+                    (subid-range (name "openwebui"))))
+             (subuids
+              (list (subid-range (name "ben"))
+                    (subid-range (name "openclaw"))
+                    (subid-range (name "openwebui"))))))
+   (service tailscale-service-type)
+   (service syncthing-service-type
+            (syncthing-configuration (user "ben")))
+   (service gnome-desktop-service-type)
+   (service qemu-binfmt-service-type
+            (qemu-binfmt-configuration
+             (platforms (lookup-qemu-platforms "arm" "aarch64"))))
+   (service bluetooth-service-type)
+   (udev-rules-service 'brightness brightnessctl)
+   (service openssh-service-type
+            (openssh-configuration
+             (permit-root-login 'prohibit-password)
+             (password-authentication? #f)
+             (authorized-keys
+              `(("root" ,(local-file "../keys/mac.pub"))
+                ("ben"  ,(local-file "../keys/mac.pub"))))))
+   (set-xorg-configuration
+    (xorg-configuration (keyboard-layout keyboard-layout))
+    sddm-service-type)
+   (service sddm-service-type
+            (sddm-configuration
+             ;; valid values are elarun, maldives or maya, chili, sugar-light
+             (theme "chili")))
+   %modified-desktop-services))
+ (bootloader (bootloader-configuration
+              (bootloader grub-efi-bootloader)
+              (targets (list "/boot/efi"))
+              (keyboard-layout keyboard-layout)))
+ (swap-devices (list (swap-space
+                      (target (uuid "893cebe6-5e30-4588-9c99-1a03389facd8")))))
+ ;; sudo blkid to list UUIDs
+ (file-systems (cons* (file-system
+                       (mount-point "/boot/efi")
+                       (device (uuid "D47F-3FB9" 'fat32))
+                       (type "vfat"))
+                      (file-system
+                       (mount-point "/")
+                       (device (uuid "d690f4db-ddfc-45e7-9b77-c4a2e08892b3" 'ext4))
+                       (type "ext4")) %base-file-systems)))
 
